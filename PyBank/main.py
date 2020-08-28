b2d8d3cbc2d682b3d72a092c6f19ec27b1db71eb
slyssy <stephen.lyssy@gmail.com>
2020-08-28 21:55:02 +0000

    Calcultated total number of months and summed profit/losses for PyBank.

---
+++ PyBank/main.py
@@ -0,0 +1,34 @@
+import os
+import csv
+
+OUT_PATH = "augmented_budget_data.csv"
+
+path = os.path.join("Resources","budget_data.csv")
+
+with open(path, "r") as file:
+    
+    csv_reader = csv.DictReader(file)
+    
+    data = list(csv_reader)
+    
+    lines = len(list(data))
+    
+    total = 0
+    
+    for row in data:
+        row = dict (row)
+        total = row["Profit/Losses"]
+        
+print(f"Total Months: {lines}")
+print(f"Total: {total}")
+      
+    
+# with open(OUT_PATH, "w+") as file:
+    
+#     csv_writer = csv.DictWriter(file, 
+#                                 [
+#                                     "Date",
+#                                     "Profit/Losses"
+                                    
+#                                 ])
+    
