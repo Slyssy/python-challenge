e515c8c628bf8a5c7e715cd5b8e7fcaced80cf61
slyssy <stephen.lyssy@gmail.com>
2020-09-03 03:24:29 +0000

    Created vote percentage variables and updated f strings.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -32,14 +32,31 @@ with open(path, "r") as file:
             OTooley = OTooley + 1
         if name == "Khan":
             Khan = Khan + 1 
-               
-    
-print(f"Total Votes: {lines}")      
-print(candidate_name)
-print("Khan:",round((Khan/lines) * 100 , 3), "% (",Khan,")")
-print("Correy:",round((Correy/lines) * 100, 3), "% (",Correy,")")
-print("Li:",round((Li/lines) * 100, 3), "% (",Li,")")
-print("O'Tooley:",round((OTooley/lines) * 100, 3), "% (",OTooley,")")
+            
+percent_kahn = round((Khan/lines) * 100 , 3)
+percent_correy = round((Correy/lines) * 100, 3)
+percent_li = round((Li/lines) * 100, 3)
+percent_otooley = round((OTooley/lines) * 100, 3)
+
+# def Winner   
+#     if Khan == max(Khan, Correy, Li, OTooley):
+#         return("Winner: Khan")
+#     elif Correy == max(Khan, Correy, Li, OTooley):
+#         return("Winner: Correy")
+#     elif Li == max(Khan, Correy, Li, OTooley):
+#         return("Winner: Li")
+#     elif OTooley == max(Khan, Correy, Li, OTooley):
+#         return("Winner: O'Tooley")
+
+print("Election Results")               
+print("_____________________________")    
+print(f"Total Votes: {lines}")
+print("_____________________________")
+print(f"Khan: {percent_kahn}% ({Khan})")
+print(f"Correy: {percent_correy}% ({Correy})")
+print(f"Li: {percent_li}% ({Li})")    
+print(f"O'Tooley: {percent_otooley}% ({OTooley})")
+print("_____________________________")
 
 if Khan == max(Khan, Correy, Li, OTooley):
     print("Winner: Khan")
@@ -49,5 +66,23 @@ elif Li == max(Khan, Correy, Li, OTooley):
     print("Winner: Li")
 elif OTooley == max(Khan, Correy, Li, OTooley):
     print("Winner: O'Tooley")
+
+# if Khan == max(Khan, Correy, Li, OTooley):
+#     print("Winner: Khan")
+# elif Correy == max(Khan, Correy, Li, OTooley):
+#     print("Winner: Correy")
+# elif Li == max(Khan, Correy, Li, OTooley):
+#     print("Winner: Li")
+# elif OTooley == max(Khan, Correy, Li, OTooley):
+#     print("Winner: O'Tooley")
     
-        
+# with open("./analysis/output.txt", "w+") as file:
+#     file.write("Election Results\n")               
+#     file.write("______________________________\n")    
+#     file.write(f"Total Votes: {lines}\n")
+#     file.write("______________________________\n")
+#     file.write("Khan:",round((Khan/lines) * 100 , 3),"% (",Khan,")\n")
+#     file.write("Correy:",round((Correy/lines) * 100, 3),"% (",Correy,")\n")
+#     file.write("Li:",round((Li/lines) * 100, 3),"% (",Li,")\n")
+#     file.write("O'Tooley:",round((OTooley/lines) * 100, 3),"% (",OTooley,")\n")
+#     file.write("______________________________\n")
